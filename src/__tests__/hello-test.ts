97693542c45047a434b7e2d78fef598be805ed8e
Kevin Milam <kevin.milam@gmail.com>
2016-04-27 14:19:22 -0500

    Configured for Jest unit testing

---
+++ src/__tests__/hello-test.ts
@@ -0,0 +1,16 @@
+
+describe('subtest', () => {
+    it('is always true', () => {
+        const trueVar = true;
+        expect(trueVar).toBe(true);
+    });
+});
+
+
+
+describe('another test', () => {
+    it('is always false', () => {
+        const trueVar = false;
+        expect(trueVar).toBe(true);
+    });
+});
--- src/tests/first.spec.ts
+++
@@ -1,8 +0,0 @@
-
-describe('Set of Tests', () => {
-
-  it('true is true', function() { expect(true).toEqual(false); });
-  it('returns the number 55', function() {
-    expect(55).toEqual(55);
-  });
-});
